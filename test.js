16ebafb5b8e40a7503b7f2c6a66973f9b21c8977
tunnckoCore <mameto_100@mail.bg>
2016-11-08 19:34:40 +0200

    test(add): implement tests

--- test.js
+++ test.js
@@ -10,7 +10,57 @@
 'use strict'
 
 var test = require('mukla')
+var plugin = require('./index')
+var MiniBase = require('minibase').MiniBase
 
-test(function (done) {
+test('should throw TypeError if `name` not a string', function (done) {
+  function fixture () {
+    plugin(123)
+  }
+  test.throws(fixture, TypeError)
+  test.throws(fixture, /expect `name` to be string/)
+  done()
+})
+
+test('should throw TypeError if `fn` not a function', function (done) {
+  function fixture () {
+    plugin('foo-bar', 123)
+  }
+  test.throws(fixture, TypeError)
+  test.throws(fixture, /expect `fn` to be function/)
+  done()
+})
+
+test('should register plugin by name to `app.registered`', function (done) {
+  var app = new MiniBase()
+  app.use(plugin('base-foo', function (self) {
+    self.foo = 'bar'
+  }))
+  test.ok(app.registered['base-foo'])
+  test.strictEqual(app._pluginName, 'base-foo')
+  test.strictEqual(app.foo, 'bar')
+
+  app.use(plugin('second-qux', function (self) {
+    self.bar = 'qux'
+  }))
+  test.ok(app.registered['second-qux'])
+  test.strictEqual(app._pluginName, 'second-qux')
+  test.strictEqual(app.bar, 'qux')
+  done()
+})
+
+test('should invoke plugin once, because using isRegistered', function (done) {
+  var base = new MiniBase()
+  var called = 0
+  function fn (opts) {
+    return plugin('foo-qux', function noop () {
+      called++
+    })
+  }
+
+  base.use(fn())
+  base.use(fn())
+
+  test.strictEqual(called, 1)
   done()
 })
