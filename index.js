aa7749399d02a2b7937107f50a3e78504732b264
tunnckoCore <mameto_100@mail.bg>
2016-11-08 19:19:55 +0200

    chore(init): scaffold

---
+++ index.js
@@ -0,0 +1,8 @@
+/*!
+ * minibase-create-plugin <https://github.com/node-minibase/minibase-create-plugin>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
---
+++ test.js
@@ -0,0 +1,16 @@
+/*!
+ * minibase-create-plugin <https://github.com/node-minibase/minibase-create-plugin>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
+
+var test = require('mukla')
+
+test(function (done) {
+  done()
+})
