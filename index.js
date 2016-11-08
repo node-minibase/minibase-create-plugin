70351b06b8a334a078bdb86931b3b1a87af0026e
tunnckoCore <mameto_100@mail.bg>
2016-11-08 19:24:56 +0200

    chore(init): implement

--- index.js
+++ index.js
@@ -6,3 +6,23 @@
  */
 
 'use strict'
+
+var isRegistered = require('minibase-is-registered')
+
+module.exports = function minibaseCreatePlugin (name, fn) {
+  if (typeof name !== 'string') {
+    throw new TypeError('minibase-create-plugin: expect `name` to be string')
+  }
+  if (typeof fn !== 'function') {
+    throw new TypeError('minibase-create-plugin: expect `fn` to be function')
+  }
+  return function plugin (self) {
+    self.use(isRegistered())
+
+    /* istanbul ignore next */
+    if (self.isRegistered(name)) return
+
+    self.use(fn)
+    return plugin
+  }
+}
